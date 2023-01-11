4338ec07d0b5a451aadc0aed710c94cd452fb131
Aryan Nimesh Mehta <aryannmehta@gmail.com>
2023-01-11 14:52:52 +0530

    Delete about.js

--- client/about.js
+++
@@ -1,4 +0,0 @@
-export const github = 'https://github.com/aryan-mehta05';
-export const linkedin = 'https://www.linkedin.com/in/aryannmehta/';
-export const instagram = 'https://www.instagram.com/ary_n_meth/';
-export const email = 'aryannmehta@gmail.com';
